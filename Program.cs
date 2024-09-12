6e71846f627fec385f2d926d26b5169c219e1cf1
Jacob Wehage <jwehage1@nait.ca>
2024-09-11 18:01:46 -0600

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿/// Summary
+/// Program function: Convert minutes into seconds and displays result
+/// Author: Jacob Wehage
+/// Date Created: Sept 11, 2024
+/// /Summary
+
+// Define second as int
+int second = 0;
+// Define minute as int
+int minute = 0;
+// Define counter as int
+int counter = 0;
+
+// Declare 1 minute being equal to 60 seconds
+
+// Prompt user to input amount of minutes
+
+// Count how many minutes the user input
+
+// multiply the amount of the counter, by the seconds in a minute
+
+// Display the result for the user
+
+// End Program
